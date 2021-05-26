2cef672e3cd565a0e8542a86cbe2b27bde485d71
Kristijan Stojcev <stojcev_kristijan@hotmail.com>
2021-05-26 18:20:49 +0200

    Added about me functionality and made changes to responsive design

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(function () {
   let contact = $("#contact");
+  let aboutMeBtn = $("#aboutMeBtn");
+  let aboutMe = $("#aboutMe");
+  // aboutMe.hide();
 
   contact.on("click", function (e) {
     e.preventDefault();
@@ -8,5 +11,9 @@ $(function () {
     } else if (contact.text() == "stojcev_kristijan@hotmail.com") {
       contact.text("contact");
     }
+  });
+
+  aboutMeBtn.on("click", function () {
+    aboutMe.toggle(500);
   });
 });
